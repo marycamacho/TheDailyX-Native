d6cec429f2b025f7c5f956d4aba811e86ad866b8
MaryC <mary@joyasolutions.com>
2017-05-07 14:45:05 -0600

    Added comment in the metricBox component to help understand what and why it is.

--- web/src/components/metricBox.js
+++ web/src/components/metricBox.js
@@ -1,5 +1,8 @@
 /**
  * Created by marycamacho on 5/2/17.
+ * This component is intended to be the smallest component in the project - the square that in each metric
+ * is either blank, has a single slash or a full X. The references to user here are left over from a previous project
+ * and can be removed. We can decide at what level user validation should go later.
  */
 
 import React, { Component } from 'react';
