be4efe3e5ecd6dcf3250b179b555c077e81045c7
Matthew Nichols <matthew@matthew-nichols.com>
2017-05-25 19:00:17 -0600

    Added React-Router and added an empty settings page.

---
+++ web/src/components/Settings.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default function() {
+
+    return (
+        <div id="settings">
+            Settings
+        </div>
+    )
+
+}
--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
 import promise from 'redux-promise';
 import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import App from './components/App';
+import './index.css';
+import reducers from './reducers';
+import Settings from "./components/Settings";
 
 const createStoreWithMiddleware = applyMiddleware(
     promise, thunk
@@ -14,7 +16,12 @@ const createStoreWithMiddleware = applyMiddleware(
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-        <App />
+        <BrowserRouter>
+            <Switch>
+                <Route path="/settings" component={Settings}/>
+                <Route path="/" component={App}/>
+            </Switch>
+        </BrowserRouter>
     </Provider>,
   document.getElementById('root')
 );
