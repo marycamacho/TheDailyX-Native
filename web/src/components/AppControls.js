fb2061c0111dc1cb1c18eb49e8a1c5ecf4cc6a81
Matthew Nichols <matthew@matthew-nichols.com>
2017-05-25 19:27:08 -0600

    Added navigation

--- web/src/components/AppControls.js
+++ web/src/components/AppControls.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { reset } from '../actions';
 
 
@@ -10,6 +11,8 @@ class AppControls extends Component {
                 <a href="#" onClick={this.props.reset.bind(this)}>
                     <i className="fa fa-refresh"></i>
                 </a>
+
+                <Link to="/settings">Settings</Link>
             </div>
         );
     }
--- web/src/components/Settings.js
+++ web/src/components/Settings.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 export default function() {
 
     return (
         <div id="settings">
             Settings
+
+            <div>
+                <Link to="/" replace={true}>Back</Link>
+            </div>
         </div>
     )
 
